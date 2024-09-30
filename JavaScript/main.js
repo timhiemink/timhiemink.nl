9c4840a188cb21ebd6a570bc09e3c2f4c8003973
timhiemink <timhiemink3@gmail.com>
2024-09-30 19:43:26 +0200

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,55 @@
+/* Make hamburger menu for phone users */
+
 var MenuClosed = true;
 
 function toggleMenu() {
     var element = document.getElementById("nav-menu-id");
     var header = document.getElementById("header-id");
     if (MenuClosed === true) { 
-        element.style.display = "flex"  
-        element.style.height = `calc(100vh - ${header.offsetHeight}px)`;
-        element.style.margin = `${header.offsetHeight}px) 0 0 0`;
+        element.style.display = "flex";
+        element.style.height = "100vh";
 
         MenuClosed = false;
-    } else {
-        element.style.display = "none"
-        element.style.height = `calc(100vh - ${header.offsetHeight}px)`;
-        element.style.margin = `${header.offsetHeight}px) 0 0 0`;
     }
-    
 }
+
+
+
+
+
+/* play music and rotate background*/ 
+const images = [
+    'url(../Images/1arrows.jpg)',
+    'url(../Images/2arrows.jpg)',
+    'url(../Images/3arrows.jpg)',
+    'url(../Images/4arrows.jpg)'
+];
+let currentIndex = 0;
+let interval;
+
+const main = document.getElementById("main")
+function changeBackground() {
+    main.style.backgroundImage = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+const button = document.getElementById('play-button');
+const audio = document.getElementById('button-audio');
+function PlayStop() {
+    button.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+            button.textContent = '| |';
+            button.classList.add('playing')
+            interval = setInterval(changeBackground, 1000);
+            /* button.classList.add('pressed'); */
+        } else {
+            audio.pause();
+            button.textContent = '▶';
+            clearInterval(interval);
+            button.classList.remove('playing')
+            /* button.classList.remove('pressed'); */
+        }
+    });
+}
+PlayStop();
